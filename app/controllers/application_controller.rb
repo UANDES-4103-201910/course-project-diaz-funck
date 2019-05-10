4eddfaf3c6cd3277208942e7a21229fbda4b22b3
Martín Díaz <mfdiaz1@miuandes.cl>
2019-05-10 05:13:20 -0400

    Added header navbar in application layout which depends on user privileges.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,22 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :null_session
+  helper_method :render_navbar
+
+  def render_navbar
+    if is_user_admin?
+      render :partial => "layouts/navbar_admin"
+    elsif is_user_logged_in?
+      render :partial => "layouts/navbar_user"
+    else
+      render :partial => "layouts/navbar_guest"
+    end
+  end
+
+  def is_user_logged_in?
+    return true
+  end
+
+  def is_user_admin?
+    return true
+  end
 end
