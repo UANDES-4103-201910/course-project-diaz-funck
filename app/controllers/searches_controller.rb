796343e4ba6d7ab13e4c4445689cd92aa281d1ed
Cristian Funck <cefunck@miuandes.cl>
2019-06-08 13:27:11 -0400

    Refactor helper method to obtain user location in searches views

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -8,18 +8,6 @@ class SearchesController < ApplicationController
     end
   end
 
-  def by_title
-    @collection = get_all_posts
-  end
-
-  def by_description
-    @collection = get_all_posts
-  end
-
-  def by_author
-    @collection = get_all_posts
-  end
-
   def user_location(user)
     location = Location.find_by id: user.location_id
     return location.region+" "+location.country
--- app/helpers/searches_helper.rb
+++ app/helpers/searches_helper.rb
@@ -1,2 +1,8 @@
 module SearchesHelper
+
+  def user_location(user)
+    location = Location.find_by id: user.location_id
+    return location.region+" "+location.country
+  end
+
 end
